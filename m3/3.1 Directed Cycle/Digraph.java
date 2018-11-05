d02de6c9897127299974c17845df6e5955160a42
gvnaakhilsurya <akhil.gonugunta@msitprogram.net>
2018-11-05 19:48:59 +0530

    "ads2_m3_a1_1: 8/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- m3/3.1 Directed Cycle/Digraph.java
+++ m3/3.1 Directed Cycle/Digraph.java
@@ -1,6 +1,27 @@
-
 import java.util.Scanner;
 import java.util.NoSuchElementException;
+
+/**
+ *  The {@code Digraph} class represents a directed graph of vertices
+ *  named 0 through <em>V</em> - 1.
+ *  It supports the following two primary operations: add an edge to the digraph,
+ *  iterate over all of the vertices adjacent from a given vertex.
+ *  Parallel edges and self-loops are permitted.
+ *  <p>
+ *  This implementation uses an adjacency-lists representation, which 
+ *  is a vertex-indexed array of {@link Bag} objects.
+ *  All operations take constant time (in the worst case) except
+ *  iterating over the vertices adjacent from a given vertex, which takes
+ *  time proportional to the number of such vertices.
+ *  <p>
+ *  For additional documentation,
+ *  see <a href="https://algs4.cs.princeton.edu/42digraph">Section 4.2</a> of
+ *  <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
+ *
+ *  @author Robert Sedgewick
+ *  @author Kevin Wayne
+ */
+
 public class Digraph {
     private static final String NEWLINE = System.getProperty("line.separator");
 
@@ -192,7 +213,4 @@ public class Digraph {
         }
         return s.toString();
     }
-
 }
-
-
