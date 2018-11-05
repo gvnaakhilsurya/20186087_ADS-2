ceab353e27eb62b9e376db71ff12d117600eca10
gvnaakhilsurya <akhil.gonugunta@msitprogram.net>
2018-11-05 22:25:30 +0530

    "ads2_m3_a2_1: 5/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- m3/3.2 Bipartite/Solution.java
+++ m3/3.2 Bipartite/Solution.java
@@ -1,20 +1,36 @@
 import java.util.Scanner;
+/**
+ * Solution class.
+ */
 public final class Solution {
-
-	private Solution() {
-
-	}
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		Graph g = new Graph(sc);
-		Bipartite bp = new Bipartite(g);
-		// System.out.println(bp.isBipartite());
-		if (bp.isBipartite() == true) {
-			System.out.println("Graph is bipartite");
-		} else {
-			System.out.println("Graph is not a bipartite");
-
-		}
-
-	}
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        /**
+         * private counstructor.
+         */
+    }
+    /**
+     * client method.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int vertices = Integer.parseInt(sc.nextLine());
+        int edges = Integer.parseInt(sc.nextLine());
+        Graph graph = new Graph(vertices);
+        while (sc.hasNext()) {
+            String[] input = sc.nextLine().split(" ");
+            graph.addEdge(Integer.parseInt(input[0]),
+                          Integer.parseInt(input[1]));
+        }
+        DirectedCycle directedcycle = new DirectedCycle(graph);
+        if (directedcycle.isBipartite()) {
+            System.out.println("Graph is bipartite");
+        } else {
+            System.out.println("Graph is not a bipartite");
+        }
+    }
 }
