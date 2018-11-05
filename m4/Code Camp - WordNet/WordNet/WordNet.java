a7bf2133a0f987e64b03426838f7cd528327a0dc
gvnaakhilsurya <akhil.gonugunta@msitprogram.net>
2018-11-06 00:09:44 +0530

    "ads2_m4_a1_1: 15/15 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- m4/Code Camp - WordNet/WordNet/WordNet.java
+++ m4/Code Camp - WordNet/WordNet/WordNet.java
@@ -1,73 +1,166 @@
+import java.util.ArrayList;
+import java.io.File;
+import java.util.HashMap;
+import java.util.Scanner;
+/**
+ * Class for word net.
+ */
 public class WordNet {
-
-    Digraph dgraph;
-
-    // constructor takes the name of the two input files
-    public WordNet(String synsets, String hypernyms) {
-        int V = processSynsets(synsets);
-        dgraph = buildGraph(hypernyms,V);
+    /**
+     * hash map.
+     */
+    private HashMap<String, ArrayList<Integer>> h = new
+    HashMap<String, ArrayList<Integer>>();
+    /**
+     * hashmap.
+     */
+    private HashMap<Integer, String> h2 = new
+    HashMap<Integer, String>();
+    /**
+     * digraph.
+     */
+    private Digraph dg;
+    /**
+     * SAP variable.
+     */
+    private SAP sap;
+    /**
+     * has cycle variable.
+     */
+    private boolean hasCycle = false;
+    /**
+     * has multiple roots variable.
+     */
+    private boolean hasMultipleRoots = false;
+    /**
+     * Gets the digraph.
+     *
+     * @return     The digraph.
+     */
+    public Digraph getDigraph() {
+        return this.dg;
     }
-    // this only used to count
-     private int processSynsets(String synsets) {
-        In in = new In (synsets);
-        int count = 0;
-        while(in.hasNextLine()) {
-            in.readLine();
-            count++;
-        }
-        return count;
+    /**
+     * get hasCycle.
+     *
+     * @return   boolean.
+     */
+    public boolean gethasCycle() {
+        return this.hasCycle;
     }
-    private Digraph buildGraph(String hypernyms,int V) {
-        Digraph dgraph = new Digraph(V);
-        In in = new In(hypernyms);
-        while(in.hasNextLine()) {
-            String[]tokens = in.readLine().split(",");
-            for (int i = 1;i < tokens.length;i++) {
-                dgraph.addEdge(Integer.parseInt(tokens[0]),
-                Integer.parseInt(tokens[i]));
+    /**
+     * hasmultipleroots.
+     *
+     * @return  boolean.
+     */
+    public boolean gethasMultipleRoots() {
+        return this.hasMultipleRoots;
+    }
+    /**
+     * Constructs the object.
+     *
+     * @param      synsets    The synsets
+     * @param      hypernyms  The hypernyms
+     */
+    public WordNet(final String synsets, final String hypernyms) {
+        try {
+            File fileOne = new File(
+                "Files/" + synsets);
+            Scanner fOne = new Scanner(fileOne);
+            File fileTwo = new File("Files/" + hypernyms);
+            Scanner fTwo = new Scanner(fileTwo);
+            while (fOne.hasNextLine()) {
+                String[] tokens = fOne.nextLine().split(",");
+                h2.put(Integer.parseInt(tokens[0]), tokens[1]);
+                String[] words = tokens[1].split(" ");
+                for (int i = 0; i < words.length; i++) {
+                    if (h.containsKey(words[i])) {
+                        ArrayList<Integer> arraylist = h.get(words[i]);
+                        arraylist.add(Integer.parseInt(tokens[0]));
+                    } else {
+                        ArrayList<Integer> arraylist = new ArrayList<Integer>();
+                        arraylist.add(Integer.parseInt(tokens[0]));
+                        h.put(words[i], arraylist);
+                    }
+                }
             }
-            
-        }
-        return dgraph;
-    }
-
-    // returns all WordNet nouns
-    public Iterable<String> nouns() {
-        return null;
-    }
-
-    // is the word a WordNet noun?
-    public boolean isNoun(String word) {
-        return false;
-    }
-
-    // distance between nounA and nounB (defined below)
-    public int distance(String nounA, String nounB) {
-        return 0;
-    }
-
-    // a synset (second field of synsets.txt) that is the common ancestor of nounA and nounB
-    // in a shortest ancestral path (defined below)
-    public String sap(String nounA, String nounB) {
-        return null;
-    }
-    public void printGraph() {
-        int count = 0;
-        for (int i = 0;i < dgraph.V();i++) {
-            if (dgraph.outdegree(i)== 0) {
-                count++;
+            dg = new Digraph(h.size());
+            while (fTwo.hasNextLine()) {
+                String[] tokens = fTwo.nextLine().split(",");
+                for (int i = 1; i < tokens.length; i++) {
+                    dg.addEdge(Integer.parseInt(tokens[0]),
+                               Integer.parseInt(tokens[i]));
+                }
             }
-        if (count>1){
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        DirectedCycle dc = new DirectedCycle(dg);
+        if (dc.hasCycle()) {
+            hasCycle = true;
+        }
+    }
+    /**
+     * check multiple roots method.
+     */
+    public void checkMultipleRoots() {
+        int roots = 0;
+        for (int i = 0; i < dg.v(); i++) {
+            if (dg.outdegree(i) == 0) {
+                roots++;
+            }
+        }
+        if (roots != 1) {
+            hasMultipleRoots = true;
             System.out.println("Multiple roots");
-            
-        }else{
-        System.out.println(dgraph);
-    }
         }
     }
-
-    // do unit testing of this class
-    public static void main(String[] args) {
-
+    /**
+     * Determines if noun.
+     *
+     * @param      word  The word
+     *
+     * @return     True if noun, False otherwise.
+     */
+    public boolean isNoun(final String word) {
+        // for (String s : h.getKeys()) {
+        //     if (s.equals(word)) {
+        //         return true;
+        //     }
+        // }
+        return true;
     }
+    /**
+     * distance.
+     *
+     * @param      nounA  The noun a
+     * @param      nounB  The noun b
+     *
+     * @return distance between nouns.
+     */
+    public int distance(final String nounA, final String nounB) {
+        ArrayList id1 = h.get(nounA);
+        ArrayList id2 = h.get(nounB);
+        sap = new SAP(dg);
+        return sap.length(id1, id2);
+    }
+    /**
+     * sap method.
+     *
+     * @param      nounA  The noun a
+     * @param      nounB  The noun b
+     *
+     * @return string.
+     */
+    public String sap(final String nounA, final String nounB) {
+        ArrayList<Integer> id1 = h.get(nounA);
+        ArrayList<Integer> id2 = h.get(nounB);
+        sap = new SAP(dg);
+        // System.out.println(id1);
+        // System.out.println(id2);
+        int ans = sap.ancestor(id1, id2);
+        return h2.get(ans);
+    }
+
 }
+
