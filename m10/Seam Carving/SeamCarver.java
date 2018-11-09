6041ed2420aaebd1f5af51c69916b155942cf689
gvnaakhilsurya <akhil.gonugunta@msitprogram.net>
2018-11-09 10:53:03 +0530

    "ads2_m10_a1_1: 3/9 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- m10/Seam Carving/SeamCarver.java
+++ m10/Seam Carving/SeamCarver.java
@@ -1,10 +1,30 @@
 
+import java.awt.Color;
 public class SeamCarver {
-	Picture picture;
-	// create a seam carver object based on the given picture
-	public SeamCarver(Picture picture1) {
-		picture = new Picture(picture1);
+	private Picture picture;
+	private double [][] energies;
 
+	// create a seam carver object based on the given picture
+	public SeamCarver(Picture picture) {
+		if (picture == null) {
+			throw new IllegalArgumentException("picture is null");
+		}
+		initSeamCarver(picture);
+	}
+
+	private void initSeamCarver(Picture picture) {
+		this.picture = new Picture(picture);
+		energies = new double[picture.width()][picture.height()];
+		// System.out.println("IN init seam carver");
+		// CalculateEnergies();
+
+	}
+	private void CalculateEnergies() {
+		for (int i= 0;i < picture.width();i++) {
+			for (int j = 0;j < picture.height();j++) {
+				energies[i][j]=energy(i,j);
+			}	
+		}
 	}
 	// current picture
 	public Picture picture() {
@@ -12,24 +32,51 @@ public class SeamCarver {
 	}
 	// width of current picture
 	public int width() {
-		return picture.width();
+		return this.picture.width();
 	}
 
 	// height of current picture
 	public int height() {
-		return picture.height();
+		return this.picture.height();
 	}
-
+	private double CaluculateDelta(Color c1,Color c2) {
+		int r = c1.getRed() - c2.getRed();
+		int g = c1.getGreen()-c2.getGreen();
+		int b = c1.getBlue()-c2.getBlue();
+		return(r * r + g * g + b * b);
+	}
+	private double CaluculateDeltaX(int x,int y) {
+		Color c1 = getPixelColor(x, y - 1);
+		Color c2 = getPixelColor(x, y + 1);
+		return CaluculateDelta(c1,c2);
+	}
+	private double CaluculateDeltaY(int x,int y) {
+		Color c1 = getPixelColor(x - 1, y);
+		Color c2 = getPixelColor(x + 1, y);
+		return CaluculateDelta(c1,c2);
+	}
+	private void valid(int x,int length){
+		if (x < 0 || x > length) {
+			throw new IllegalArgumentException("IllegalArgumentException");
+			}
+	}
+	private Color getPixelColor(int x,int y){
+		valid(x,picture.width()-1);
+		valid(y,picture.height()-1);
+		return picture.get(x,y);
+	}
 	// energy of pixel at column x and row y
 	public double energy(int x, int y) {
 		if (x == 0||y==0|| y == picture.height()-1||
-			x == picture.width()-1){
+			x == picture.width()-1) {
 			return 1000;
-
+		} else {
+			System.out.println();
+			double deltaX = CaluculateDeltaX(x,y);
+			double deltaY = CaluculateDeltaY(x,y);
+			return Math.sqrt(deltaX+deltaY);
 		}
-		return 0;
 	}
-
 	// sequence of indices for horizontal seam
 	public int[] findHorizontalSeam() {
 		return new int[0];
--- m10/Seam Carving/Solution.java
+++ m10/Seam Carving/Solution.java
@@ -10,11 +10,13 @@ public class Solution {
         SeamCarver sc = new SeamCarver(picture);
 
         StdOut.printf("Printing energy calculated for each pixel.\n");
-
+        try {
         for (int row = 0; row < sc.height(); row++) {
             for (int col = 0; col < sc.width(); col++)
                 StdOut.printf("%9.0f ", sc.energy(col, row));
             StdOut.println();
+        } } catch(Exception ex) {
+            ex.printStackTrace();
         }
     }
 
@@ -123,7 +125,7 @@ public class Solution {
                 break;
             }
         } catch (Exception ex) {
-            System.out.println("picture is null");
+            System.out.println(ex.getMessage());
         }
     }
 }
