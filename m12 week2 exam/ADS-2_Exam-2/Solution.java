1673c937d6a3e205a3ee97e10dd7a78fe244e288
gvnaakhilsurya <akhil.gonugunta@msitprogram.net>
2018-11-10 12:59:22 +0530

    "ads2_m12_a1_1: 5/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- m12 week2 exam/ADS-2_Exam-2/Solution.java
+++ m12 week2 exam/ADS-2_Exam-2/Solution.java
@@ -47,27 +47,21 @@ public final class Solution {
 		case "DirectedPaths":
 			String[] items = sc.nextLine().split(" ");
 			for (int k = 0; k <items.length; k++) {
-            DijkstraSP dsp = new DijkstraSP(graph,Integer.parseInt(items[0]));
+            DijkstraSP dsp = new DijkstraSP(graph, Integer.parseInt(items[0]));
              if (dsp.hasPathTo((Integer.parseInt(items[1])))== false) {
              	System.out.println("No Path Found.");
-             	}
-             	else{
+             	} else {
              		System.out.println(dsp.distTo(Integer.parseInt(items[1])));
              	}
              	break;
         }
 			break;
 		case "ViaPaths":
-			// Handle the case of ViaPaths, where three integers are given.
-			// First is the source and second is the via is the one where path should pass throuh.
-			// third is the destination.
-			// If the path exists print the distance between them.
-			// Other wise print "No Path Found."
 			String[] objects = sc.nextLine().split(" ");
-			for (int k = 0; k <objects.length; k++) {
+			for (int k = 0; k < objects.length; k++) {
             DijkstraSP dsp = new DijkstraSP(graph,Integer.parseInt(objects[0]));
-            if (dsp.hasPathTo((Integer.parseInt(objects[1])))&&
-            	dsp.hasPathTo((Integer.parseInt(objects[2])))== false){
+            if (dsp.hasPathTo((Integer.parseInt(objects[1])))
+            	&& dsp.hasPathTo((Integer.parseInt(objects[2]))) == false) {
             	System.out.println("No Path Found.");
             }
             break;
