d693d886e9f5f7081cf2d92ccde43942766f189c
gvnaakhilsurya <akhil.gonugunta@msitprogram.net>
2018-11-10 11:50:49 +0530

    "ads2_m12_a1_1: 2/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- m12 week2 exam/ADS-2_Exam-2/Solution.java
+++ m12 week2 exam/ADS-2_Exam-2/Solution.java
@@ -1,25 +1,34 @@
-import java.util.Scanner;
+import java.util.*;
+/**
+ * Class for solution.
+ */
 public class Solution {
 	private Solution() {
 
 	}
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int nvcities = Integer.parseInt(sc.nextLine());
-		int veroads = Integer.parseInt(sc.nextLine());
-		// System.out.println(veroads);
-		EdgeWeightedGraph graph = new EdgeWeightedGraph(nvcities);
-		for (int i = 0;i < nvcities;i++) {
-			String[]contains = sc.nextLine().split(" ");
-			graph.addEdge(new Edge(Integer.parseInt(contains[0]),
-				Integer.parseInt(contains[1]),Integer.parseInt(contains[2])));
-        }
-        String caseToGo =sc.nextLine();
-        
+		// Self loops are not allowed...
 		// Parallel Edges are allowed...
 		// Take the Graph input here...
+		Scanner sc = new Scanner(System.in);
+		int nvcities = Integer.parseInt(sc.nextLine());
+		int lines = Integer.parseInt(sc.nextLine());
+		EdgeWeightedGraph graph = new EdgeWeightedGraph(nvcities);
+		for(int i = 0; i < lines; i++) {
+			String[] tokens = sc.nextLine().split(" ");
+			graph.addEdge(new Edge(Integer.parseInt(tokens[0]),
+				Integer.parseInt(tokens[1]),
+				 Double.parseDouble(tokens[2])));
+		}
+		String caseToGo = sc.nextLine();
+		// String[]tokens = sc.nextLine().split(" ");
+		// DijkstraSP dsp = new DijkstraSP(graph);
+		// for (int j = 0;j < tokens.length;j++) {
+		// 	DijkstraSP dsp = new DijkstraSP(graph,Integer.parseInt(tokens[0]));
+		// }
 		switch (caseToGo) {
 		case "Graph":
+			//Print the Graph Object.
 			System.out.println(graph);
 			break;
 
@@ -28,6 +37,15 @@ public class Solution {
 			// First is the source and second is the destination.
 			// If the path exists print the distance between them.
 			// Other wise print "No Path Found."
+			String[] items = sc.nextLine().split(" ");
+			for (int k = 0; k <items.length; k++) {
+            DijkstraSP dsp = new DijkstraSP(graph,Integer.parseInt(items[0]));
+
+             if (dsp.hasPathTo((Integer.parseInt(items[k])))== false) {
+             	System.out.println("path not found");
+             	
+             }
+        }
 			break;
 
 		case "ViaPaths":
