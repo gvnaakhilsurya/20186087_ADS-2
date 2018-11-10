8a97ccef5f69a52448aacad54d81ff2a45450d75
gvnaakhilsurya <akhil.gonugunta@msitprogram.net>
2018-11-10 11:13:27 +0530

    "ads2_m12_a1_1: 1/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- m12 week2 exam/ADS-2_Exam-2/Solution.java
+++ m12 week2 exam/ADS-2_Exam-2/Solution.java
@@ -7,38 +7,40 @@ public class Solution {
 		Scanner sc = new Scanner(System.in);
 		int nvcities = Integer.parseInt(sc.nextLine());
 		int veroads = Integer.parseInt(sc.nextLine());
+		// System.out.println(veroads);
 		EdgeWeightedGraph graph = new EdgeWeightedGraph(nvcities);
 		for (int i = 0;i < nvcities;i++) {
 			String[]contains = sc.nextLine().split(" ");
 			graph.addEdge(new Edge(Integer.parseInt(contains[0]),
 				Integer.parseInt(contains[1]),Integer.parseInt(contains[2])));
         }
-        System.out.println(graph);
+        String caseToGo =sc.nextLine();
+        
 		// Parallel Edges are allowed...
 		// Take the Graph input here...
-		// switch (caseToGo) {
-		// case "Graph":
-		// 	//Print the Graph Object.
-		// 	break;
+		switch (caseToGo) {
+		case "Graph":
+			System.out.println(graph);
+			break;
 
-		// case "DirectedPaths":
-		// 	// Handle the case of DirectedPaths, where two integers are given.
-		// 	// First is the source and second is the destination.
-		// 	// If the path exists print the distance between them.
-		// 	// Other wise print "No Path Found."
-		// 	break;
+		case "DirectedPaths":
+			// Handle the case of DirectedPaths, where two integers are given.
+			// First is the source and second is the destination.
+			// If the path exists print the distance between them.
+			// Other wise print "No Path Found."
+			break;
 
-		// case "ViaPaths":
-		// 	// Handle the case of ViaPaths, where three integers are given.
-		// 	// First is the source and second is the via is the one where path should pass throuh.
-		// 	// third is the destination.
-		// 	// If the path exists print the distance between them.
-		// 	// Other wise print "No Path Found."
-		// 	break;
+		case "ViaPaths":
+			// Handle the case of ViaPaths, where three integers are given.
+			// First is the source and second is the via is the one where path should pass throuh.
+			// third is the destination.
+			// If the path exists print the distance between them.
+			// Other wise print "No Path Found."
+			break;
 
-		// default:
-		// 	break;
-		// }
+		default:
+			break;
+		}
 
 	}
 }
