06b4a2766a54de4fdab75d5f34354de55d2f109d
gvnaakhilsurya <akhil.gonugunta@msitprogram.net>
2018-11-11 18:26:39 +0530

    "ads2_m7_a1_1: 0/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- m7/1. MST/Edge.java
+++ m7/1. MST/Edge.java
@@ -1,70 +1,77 @@
-/**
+/**.
  * Class for edge.
  */
-public class Edge implements Comparable<Edge> {
-    /**
-     * vertex 1.
+class Edge implements Comparable<Edge> {
+    /**.
+     * Integer value.
      */
     private final int v;
-    /**
-     * vertex 2.
+    /**.
+     * integer value
      */
     private final int w;
-    /**
-     * weight.
+    /**.
+     * double value
      */
     private final double weight;
-
-    /**
-     * Initializes an edge between vertices {@code v} and {@code w} of
-     * the given {@code weight}.
+    /**.
+     * Initializes an edge between vertices {@code v} and {@code w} of the
+     * given {@code weight}.
+      *By the analysis the the time constant of the following method
+      *is O(1).
+      *The statment of the method executes only once when the method calls.
      *
-     * @param  v1 one vertex
-     * @param  w1 the other vertex
-     * @param  weight1 the weight of this edge
-     * @throws IllegalArgumentException if either {@code v} or {@code w}
-     *         is a negative integer
-     * @throws IllegalArgumentException if {@code weight} is {@code NaN}
+     * @param      vert       The vertical
+     * @param      otherVert  The other vertical
+     * @param      wt         the other vertex
+     * @throws     IllegalArgumentException  if negative integer
+     * @throws     IllegalArgumentException  if {@code weight} is {@code NaN}
      */
-    public Edge(final int v1, final int w1, final double weight1) {
-        if (v1 < 0) {
+    Edge(final int vert, final int otherVert, final double wt) {
+        if (vert < 0) {
+            throw new IllegalArgumentException(
+            "vertex index must be a nonnegative integer");
+        }
+        if (otherVert < 0) {
             throw new IllegalArgumentException(
                 "vertex index must be a nonnegative integer");
         }
-        if (w1 < 0) {
-            throw new IllegalArgumentException(
-                "vertex index must be a nonnegative integer");
-        }
-        if (Double.isNaN(weight1)) {
+        if (Double.isNaN(wt)) {
             throw new IllegalArgumentException("Weight is NaN");
         }
-        this.v = v1;
-        this.w = w1;
-        this.weight = weight1;
+        this.v = vert;
+        this.w = otherVert;
+        this.weight = wt;
     }
 
-    /**
+    /**.
      * Returns the weight of this edge.
-     *
+      *By the analysis the the time constant of the following method
+      *is O(1).
+      *The statment of the method executes only once when the method calls.
      * @return the weight of this edge
      */
     public double weight() {
         return weight;
     }
 
-    /**
+    /**.
      * Returns either endpoint of this edge.
-     *
+     *By the analysis the the time constant of the following method
+     *is O(1).
+     *The statment of the method executes only once when the method calls.
      * @return either endpoint of this edge
      */
     public int either() {
         return v;
     }
 
-    /**
-     * Returns the endpoint of this
-     * edge that is different from the given vertex.
-     *
+    /**.
+     * Returns the endpoint of this edge that is
+     * different from the given vertex.
+      *By the analysis the the time constant of the following method
+      *is O(1).
+      *The statment of the method executes only once when the method calls.
      * @param  vertex one endpoint of this edge
      * @return the other endpoint of this edge
      * @throws IllegalArgumentException if the vertex is not one of the
@@ -80,16 +87,19 @@ public class Edge implements Comparable<Edge> {
         }
     }
 
-    /**
+    /**.
      * Compares two edges by weight.
      * Note that {@code compareTo()} is not consistent with {@code equals()},
-     * which uses the reference equality
-     * implementation inherited from {@code Object}.
-     *
+     * which uses the reference equality implementation
+     *                      inherited from {@code Object}.
+      *By the analysis the the time constant of the following method
+      *is O(1).
+      *The statment of the method executes only once when the method calls.
      * @param  that the other edge
-     * @return a negative integer, zero,
-     * or positive integer depending on whether
-     *         the weight of this is less than, equal to, or greater than the
+     * @return a negative integer, zero, or positive integer
+     *         depending on whether
+     *         the weight of this is less than, equal to,
+     *         or greater than the
      *         argument edge
      */
     @Override
@@ -97,7 +107,7 @@ public class Edge implements Comparable<Edge> {
         return Double.compare(this.weight, that.weight);
     }
 
-    /**
+    /**.
      * Returns a string representation of this edge.
      *
      * @return a string representation of this edge
@@ -105,9 +115,7 @@ public class Edge implements Comparable<Edge> {
     public String toString() {
         return String.format("%d-%d %.5f", v, w, weight);
     }
-
 }
-
 
 
 
